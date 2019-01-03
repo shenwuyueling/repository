5a11a5e458507f272b9ab13c0b1bd9b699c53eea
shenwuyueling <yue_xin_001@sohu.com>
2019-01-03 19:09:44 +0800

    0.0.2-SNAPSHOT

---
+++ alearn/src/main/java/com/thtf/alearn/alearn/App.java
@@ -0,0 +1,13 @@
+package com.thtf.alearn.alearn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
